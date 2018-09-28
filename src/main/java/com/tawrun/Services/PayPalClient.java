e2175058f13a7219dd7c992d5698c4a07b1b2acc
Akshay Sharma <contact@imakshay.com>
2018-09-28 17:55:47 +0530

    begin adding paypal client

---
+++ src/main/java/com/tawrun/Services/PayPalClient.java
@@ -0,0 +1,65 @@
+package com.tawrun.Services;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+client secret: EEDSEjTkEe7pYE74R_7AZI-njS3bpo3P8IhP36elsFN5p26xTk4g1FEVos_3ZwZYee6drp0IOy1OpPuG
+client ID: AWkwWxlmZXEEUJ8wvF5wYv71w9dr94RnM4gRPNqcqyc_8paJsov_oGcHDn1KpPd0gKsYulGHYHJZdsms
+sandbox account: sushsilsharma.ss451-facilitator@gmail.com
+ */
+
+public class PayPalClient {
+    String clientId = "AWkwWxlmZXEEUJ8wvF5wYv71w9dr94RnM4gRPNqcqyc_8paJsov_oGcHDn1KpPd0gKsYulGHYHJZdsms";
+    String clientSecret = "EEDSEjTkEe7pYE74R_7AZI-njS3bpo3P8IhP36elsFN5p26xTk4g1FEVos_3ZwZYee6drp0IOy1OpPuG";
+
+    public Map<String, Object> createPayment(String sum){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(sum);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:4200/cancel");
+        redirectUrls.setReturnUrl("http://localhost:4200/");
+        payment.setRedirectUrls(redirectUrls);
+        Payment createdPayment;
+        try {
+            String redirectUrl = "";
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            createdPayment = payment.create(context);
+            if(createdPayment!=null){
+                List<Links> links = createdPayment.getLinks();
+                for (Links link:links) {
+                    if(link.getRel().equals("approval_url")){
+                        redirectUrl = link.getHref();
+                        break;
+                    }
+                }
+                response.put("status", "success");
+                response.put("redirect_url", redirectUrl);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println("Error happened during payment creation!");
+        }
+        return response;
+    }
+}
