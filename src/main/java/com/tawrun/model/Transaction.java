0a16093b54fd61cd5b4b070a365627f0fd67bbfb
Akshay Sharma <contact@imakshay.com>
2018-09-30 16:02:26 +0530

    create Payment, Transaction, PayInfo models

---
+++ src/main/java/com/tawrun/model/Transaction.java
@@ -0,0 +1,18 @@
+package com.tawrun.model;
+
+public class Transaction {
+
+    // TODO define it
+    public Transaction(){
+
+    }
+
+    // TODO define it
+    public PayInfo toPayInfo(){
+        PayInfo p;
+
+        // Add logic
+
+        return p;
+    }
+}
